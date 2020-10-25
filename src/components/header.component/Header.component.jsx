ed6adf1d64c8e76b666abc10566f8b4c299a64c0
faldi kusuma <ifaltiga.pm@gmail.com>
2020-10-26 06:38:21 +0700

    make hompage and directory

---
+++ src/components/header.component/Header.component.jsx
@@ -0,0 +1,9 @@
+import './Header.component.styles';
+import { Link } from 'react-router-dom';
+const Header = () => (
+  <div className="header">
+    <Link className="logo-container"></Link>
+  </div>
+);
+
+export default Header;
