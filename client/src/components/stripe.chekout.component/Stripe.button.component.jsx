c11807b81f07500b7c615cf542843de1d4228ea2
kusuma-repo <ifaltiga.pm@gmail.com>
2020-11-22 21:38:39 +0700

    seperating cra into client folder and make server for bakcend

--- client/src/components/stripe.chekout.component/Stripe.button.component.jsx
+++ client/src/components/stripe.chekout.component/Stripe.button.component.jsx
@@ -1,11 +1,28 @@
 import StripeCheckout from 'react-stripe-checkout';
 
+import axios from 'axios';
+
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     'pk_test_51Hd6VDDxAX8ghbPPSn8rA1agCDNP4J8CS8mhOOx5E9kzGAhlaRW8BVhLsZyoVE0xN4r4vyub9koRH97DfSpmw3Gl00UsnQTiUc';
   const onToken = (token) => {
-    alert('Payment Succesful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => {
+        alert('Payment Succesful');
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log('payment error', JSON.parse(error));
+        alert('somenthing wrong with the payment please check the credit cart');
+      });
   };
   return (
     <StripeCheckout
