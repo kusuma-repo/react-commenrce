14401258e46b6c3155b12d1ac791540e45268d30
kusuma-repo <ifaltiga.pm@gmail.com>
2020-12-05 06:08:04 +0700

    fix route server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.listen(port, (error) => {
   if (error) throw error;
   console.log(`server running on port ${port}`);
 });
-app.get('./service-worker.js', (req, res) => {
+app.get('/service-worker.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 app.post('/payment', (req, res) => {
